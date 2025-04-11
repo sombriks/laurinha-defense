879a9581fb247f85dd3dc7d38fe0bf42752a8e68
Leonardo Silveira <sombriks@gmail.com>
2025-04-11 11:06:18 -0300

    testing shapes

--- src/js/scenes/intro.js
+++ src/js/scenes/intro.js
@@ -2,4 +2,21 @@ import Phaser from "phaser"
 
 export class Intro extends Phaser.Scene {
 
+  #sprites = []
+
+  create() {
+    const graphics = this.add.graphics({lineStyle: {color: 0xff0000}})
+
+    const circle = new Phaser.Geom.Circle(400, 300, 100)
+    const line = new Phaser.Geom.Line(400, 400, 400, 300)
+
+    graphics.strokeCircleShape(circle)
+    graphics.strokeLineShape(line)
+
+    this.#sprites.push(graphics)
+  }
+
+  update(time, delta) {
+    Phaser.Actions.IncY(this.#sprites, 0.05)
+  }
 }
